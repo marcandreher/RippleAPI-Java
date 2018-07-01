00ee800d3eb323d5cea1c2bbdd51f91283fcf2cf
MarcPlaying <herpersmarcandre@gmail.com>
2018-07-01 13:14:52 +0200

    CodeFactor improvments at the onlineUserChecker

--- api/API/onlineUserChecker.java
+++ api/API/onlineUserChecker.java
@@ -17,32 +17,8 @@ public class onlineUserChecker {
 	private Long result;
 	private String message;
 	
-	public onlineUserChecker() {
-		try {
-			 String url = "https://c."+Static.server + "/api/v1/onlineUsers";
-		     URL obj = new URL(url);
-		     HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-		     con.setRequestMethod("GET");
-		     con.setRequestProperty("User-Agent", "Mozilla/5.0");
-		     @SuppressWarnings("unused")
-			int responseCode = con.getResponseCode();
-		     BufferedReader in = new BufferedReader(
-		             new InputStreamReader(con.getInputStream()));
-		     String inputLine;
-		     StringBuffer response = new StringBuffer();
-		     while ((inputLine = in.readLine()) != null) {
-		     	response.append(inputLine);
-		     }
-		     in.close();
-		     JSONParser parser = new JSONParser();
-		     Object obj2 = parser.parse(response.toString());
-		     JSONObject jsonObject = (JSONObject) obj2;
-		     status = (Long) jsonObject.get("status");
-		     result = (Long) jsonObject.get("result");
-		     message = (String) jsonObject.get("message");
-		    }catch(Exception e) {
-				System.out.println("[RippleAPI]: Error");
-			}
+	public onlineUserChecker() throws Exception {
+		getItems();
 	}
 
 	public Long getResult() {
@@ -57,32 +33,30 @@ public class onlineUserChecker {
 		return message;
 	}
 	
-	public void Refresh() {
-		try {
-			 String url = "https://c."+Static.server + "/api/v1/onlineUsers";
-		     URL obj = new URL(url);
-		     HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-		     con.setRequestMethod("GET");
-		     con.setRequestProperty("User-Agent", "Mozilla/5.0");
-		     @SuppressWarnings("unused")
-			int responseCode = con.getResponseCode();
-		     BufferedReader in = new BufferedReader(
-		             new InputStreamReader(con.getInputStream()));
-		     String inputLine;
-		     StringBuffer response = new StringBuffer();
-		     while ((inputLine = in.readLine()) != null) {
-		     	response.append(inputLine);
-		     }
-		     in.close();
-		     JSONParser parser = new JSONParser();
-		     Object obj2 = parser.parse(response.toString());
-		     JSONObject jsonObject = (JSONObject) obj2;
-		     status = (Long) jsonObject.get("status");
-		     result = (Long) jsonObject.get("result");
-		     message = (String) jsonObject.get("message");
-		    }catch(Exception e) {
-				System.out.println("[RippleAPI]: Error");
-			}
+	public void Refresh() throws Exception {
+		getItems();
 	}
-
+	
+	private void getItems() throws Exception
+	{
+		 String server = "https://c."+Static.server + "/api/v1/onlineUsers";
+	     URL objurl = new URL(server);
+	     HttpsURLConnection connect = (HttpsURLConnection) objurl.openConnection();
+	     connect.setRequestMethod("GET");
+	     connect.setRequestProperty("User-Agent", "Mozilla/5.0");
+	     BufferedReader in = new BufferedReader(
+	             new InputStreamReader(connect.getInputStream()));
+	     String inputLine;
+	     StringBuffer response = new StringBuffer();
+	     while ((inputLine = in.readLine()) != null) {
+	     	response.append(inputLine);
+	     }
+	     in.close();
+	     JSONParser parser = new JSONParser();
+	     Object obj2 = parser.parse(response.toString());
+	     JSONObject jsonObject = (JSONObject) obj2;
+	     status = (Long) jsonObject.get("status");
+	     result = (Long) jsonObject.get("result");
+	     message = (String) jsonObject.get("message");
+	}
 }
